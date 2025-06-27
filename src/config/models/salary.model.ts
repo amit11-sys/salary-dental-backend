c47a9a3e7628a4ef4f11dcdf7eb49d7fd5274806
Amit <amittag27@gmail.com>
2025-06-27 15:14:22 +0530

    switched to previous keys

--- src/config/models/salary.model.ts
+++ src/config/models/salary.model.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const salarySchema = new mongoose.Schema({
-  annual_base_salary: { type: Number },
+  base_salary: { type: Number },
   bonus: { type: Number, },
   chooseSpecialty: {
     type: String,
@@ -13,19 +13,20 @@ const salarySchema = new mongoose.Schema({
   //   required: true,
   //   match: /.+\@.+\..+/, // basic email pattern
   // },
-  average_hours_per_week: { type: Number },
-  practice_setting : { type: String },
-  paid_time_off_weeks  : { type: Number, },
+  hoursWorked: { type: Number },
+  practiceSetting : { type: String },
+   ptoWeeks  : { type: Number, },
   rating: { type: Number, min: 1, max: 5 },
-  job_satisfaction_level  : { type: String },
+  satisfactionLevel  : { type: String },
   specialty  : { type: String },
   sub_specialty: { type: String },
   state: { type: String },
   would_choose_specialty_again  : { type: String },
-  years_of_experience  : { type: Number },
+  yearsOfExperience  : { type: Number },
   insights_improvement :{type:String},
   insights_work_life_balance  :{type:String},
-  production_percentage  :{type:String}
+  production_percentage  :{type:String},
+   compensation_type  :{type:String}
 }, {
   timestamps: true,
 });
